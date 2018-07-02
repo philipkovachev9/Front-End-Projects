17a926cfd81653f23682dc5b5ab08838859b04c8
philipkovachev9 <philipkovachev9@yahoo.com>
2018-07-02 11:35:46 +0300

    The dropdown menu can now close on mouseout

--- Green Iguana Foundation Project/scripts/main.js
+++ Green Iguana Foundation Project/scripts/main.js
@@ -1,5 +1,12 @@
-$(document).ready(function () {
-    $('#menu').hover(function () {
-        $('#dropdown').addClass('active');
-    });
+var closeDropdown = function(){
+    $('#dropdown').hide();
+}
+
+$('#menu').on('mouseover', function(e){ 
+    $('#dropdown').show().addClass('active');
+    clearTimeout(timeout);
+});
+
+$('#menu').on('mouseout', function(e){ 
+     timeout = setTimeout(closeDropdown, 1000);
 });
