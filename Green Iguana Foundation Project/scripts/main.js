875ac790d611821f6ba3955c82d21f660083638a
philipkovachev9 <philipkovachev9@yahoo.com>
2018-07-03 23:36:48 +0300

    For the sake of easier code management I seperated my image slider logic into a seperate file called slider and added the hover class to the other pages

--- Green Iguana Foundation Project/scripts/main.js
+++ Green Iguana Foundation Project/scripts/main.js
@@ -8,25 +8,5 @@ $('#menu').on('mouseover', function(){
 });
 
 $('#menu').on('mouseout', function(){ 
-     timeout = setTimeout(closeDropdown, 500);
+     timeout = setTimeout(closeDropdown, 700);
 });
-
-var imageArray = [
-    './images/image-slider1.jpg',
-    './images/image-slider2.jpg',
-    './images/image-slider3.jpg',
-];
-
-var index = 0;
-imageDuration = 6000;
-
-function slideShow() {
-    document.getElementById('image').src = imageArray[index];
-    index++;
-    if(index == imageArray.length) {
-     index = 0;
-    }
-setTimeout('slideShow()', imageDuration);
-}
-
-slideShow();
