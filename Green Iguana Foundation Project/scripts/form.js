b63187e765e875a3f371aac6c1f630b1d6ec3e43
philipkovachev9 <philipkovachev9@yahoo.com>
2018-07-04 15:16:13 +0300

    Added some basic validation to the form

--- Green Iguana Foundation Project/scripts/form.js
+++ Green Iguana Foundation Project/scripts/form.js
@@ -1,2 +1,9 @@
-
+function validate() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('mail').value;
+    var message = document.getElementById('msg').value;
+    if (name == "" || email == "" || message == "") {
+        alert("All fields are required");
+    }
+}
     
