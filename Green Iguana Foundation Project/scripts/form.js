13886b986109de2c5b5e073829cdf1a639a663ea
philipkovachev9 <philipkovachev9@yahoo.com>
2018-07-05 14:50:51 +0300

    Form validation is ready

--- Green Iguana Foundation Project/scripts/form.js
+++ Green Iguana Foundation Project/scripts/form.js
@@ -3,6 +3,10 @@ function validate() {
     var email = document.getElementById('mail').value;
     var message = document.getElementById('msg').value;
     if (name == "" || email == "" || message == "") {
-        alert("All fields are required");
+        var error = document.createElement('p').textContent = "*This Field is Required";
+        document.getElementById('error_name').innerHTML = error;
+        document.getElementById('error_email').innerHTML = error;
+        document.getElementById('error_msg').innerHTML = error;
+        event.preventDefault();
     }
 }
