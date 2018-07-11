dc887f45c63222f0b7e87b3777195d9f58e5ee88
Philip Kovachev <pkovatc@training.local>
2018-07-11 18:29:34 +0300

    Refactored the form.js file by using only single quotes

--- Green Iguana Foundation Project/scripts/form.js
+++ Green Iguana Foundation Project/scripts/form.js
@@ -4,8 +4,8 @@ function validate() {
     var name = document.getElementById('name').value;
     var email = document.getElementById('mail').value;
     var message = document.getElementById('msg').value;
-    if (name == "" || email == "" || message == "") {
-        var error = document.createElement('p').textContent = "*This Field is Required";
+    if (name == '' || email == '' || message == '') {
+        var error = document.createElement('p').textContent = '*This Field is Required';
         document.getElementById('error_name').innerHTML = error;
         document.getElementById('error_email').innerHTML = error;
         document.getElementById('error_msg').innerHTML = error;
