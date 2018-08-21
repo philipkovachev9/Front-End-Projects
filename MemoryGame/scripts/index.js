2055ca2f4b415078260f7a3acf7072c7418d2231
philipkovachev9 <philipkovachev9@yahoo.com>
2018-08-21 11:27:55 +0300

    Only two cards can be selected

--- MemoryGame/scripts/index.js
+++ MemoryGame/scripts/index.js
@@ -1,73 +1,83 @@
 const cardsArray = [
     {
         'name': 'card1',
-        'img' : './images/1.jpeg',
+        'img': './images/1.jpeg',
     },
     {
         'name': 'card2',
-        'img' : './images/2.jpeg',
+        'img': './images/2.jpeg',
     },
     {
         'name': 'card3',
-        'img' : './images/3.jpg',
+        'img': './images/3.jpg',
     },
     {
         'name': 'card4',
-        'img' : './images/4.jpeg',
+        'img': './images/4.jpeg',
     },
     {
         'name': 'card5',
-        'img' : './images/5.jpg',
+        'img': './images/5.jpg',
     },
     {
         'name': 'card6',
-        'img' : './images/6.jpg',
+        'img': './images/6.jpg',
     },
     {
         'name': 'card7',
-        'img' : './images/7.jpeg',
+        'img': './images/7.jpeg',
     },
     {
         'name': 'card8',
-        'img' : './images/8.jpg',
+        'img': './images/8.jpg',
     },
     {
         'name': 'card9',
-        'img' : './images/9.jpeg',
+        'img': './images/9.jpeg',
     },
     {
         'name': 'card10',
-        'img' : './images/10.jpg',
+        'img': './images/10.jpg',
     },
     {
         'name': 'card11',
-        'img' : './images/11.jpeg',
+        'img': './images/11.jpeg',
     },
     {
         'name': 'card12',
-        'img' : './images/12.jpg',
+        'img': './images/12.jpg',
     },
 ]
-// Concatinating array, sorting it and adding classes
+
 let gameGrid = cardsArray.concat(cardsArray);
 gameGrid.sort(() => 0.5 - Math.random());
+
+let count = 0;
+
+// Grab the root element, create a grid, set the class grid and append it
 const game = document.getElementById('game');
 const grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
 game.appendChild(grid);
 
-// Foreaching and displaying items from the array
+//For each items in the array, create a card, change the class and dataset, then append it
 gameGrid.forEach(item => {
-    const card = document.createElement('div')
-    card.classList.add('card');
-    card.dataset.name = item.name;
-    card.style.backgroundImage = `url(${item.img})`;
-    grid.appendChild(card);
-  });
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = item.name;
+  card.style.backgroundImage = `url(${item.img})`;
+  grid.appendChild(card);
+});
 
-  grid.addEventListener('click', function(event) {
-    let clicked = event.target;
-    // Do not allow the grid section to be selected, only the divs inside
-    if(clicked.nodeName === 'SECTION') {return}
+//Create an onclick event, match only two cards
+grid.addEventListener('click', function (event) {
+  let clicked = event.target;
+  if (clicked.nodeName === 'SECTION') { return; }
+  if (count < 2) {
+    count++;
     clicked.classList.add('selected');
-  });
+  }
+});
+
+
+  
