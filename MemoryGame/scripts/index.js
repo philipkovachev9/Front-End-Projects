592a958f06401103e5b9d713a177b26f37e15929
philipkovachev9 <philipkovachev9@yahoo.com>
2018-08-21 11:14:50 +0300

    Cards can now be selected

--- MemoryGame/scripts/index.js
+++ MemoryGame/scripts/index.js
@@ -48,38 +48,26 @@ const cardsArray = [
         'img' : './images/12.jpg',
     },
 ]
-// Copying each card
+// Concatinating array, sorting it and adding classes
 let gameGrid = cardsArray.concat(cardsArray);
-
-// Randomizing the array
 gameGrid.sort(() => 0.5 - Math.random());
-
-// Grabbing the div with id of root
 const game = document.getElementById('game');
-
-// Creating a grid and setting a class of grid
 const grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
-
-//Appending the grid section to the game div
 game.appendChild(grid);
 
-// For each item in the cardsArray array...
+// Foreaching and displaying items from the array
 gameGrid.forEach(item => {
-    // Create a div
-    const card = document.createElement('div');
-  
-    // Apply a card class to that div
+    const card = document.createElement('div')
     card.classList.add('card');
-  
-    // Set the data-name attribute of the div to the cardsArray name
     card.dataset.name = item.name;
-  
-    // Apply the background image of the div to the cardsArray image
     card.style.backgroundImage = `url(${item.img})`;
-  
-    // Append the div to the grid section
     grid.appendChild(card);
   });
 
-  
+  grid.addEventListener('click', function(event) {
+    let clicked = event.target;
+    // Do not allow the grid section to be selected, only the divs inside
+    if(clicked.nodeName === 'SECTION') {return}
+    clicked.classList.add('selected');
+  });
