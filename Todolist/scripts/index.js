dd5c50132dc574ce9b852f616965d00f31ee575e
philipkovachev9 <philipkovachev9@yahoo.com>
2018-08-22 20:15:55 +0300

    Refactored the event listener

--- Todolist/scripts/index.js
+++ Todolist/scripts/index.js
@@ -1,17 +1,19 @@
-function addTodo() {
+    var button = document.getElementById('add-button');
+    button.addEventListener('click', function () {
+            
     var item = document.getElementById('input').value;
     var text = document.createTextNode(item);
     var newItem = document.createElement('li');
     newItem.appendChild(text);
     document.getElementById('todoList').appendChild(newItem);
-
+        
     var removeTask = document.createElement('img');
     removeTask.setAttribute('src', '/images/trash.jpg');
     removeTask.setAttribute('id', 'trash');
-
     removeTask.addEventListener('click', function() {
         newItem.parentNode.removeChild(newItem);
     });
     newItem.appendChild(removeTask);
-}
+    
+});
 
