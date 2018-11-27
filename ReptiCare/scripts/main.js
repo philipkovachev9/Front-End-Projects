1c143b2741b47676bc07909f1b88fe19840dad17
Philip Kovachev <pkova@softserveinc.com>
2018-11-27 08:29:49 +0200

    Created the edit form

--- ReptiCare/scripts/main.js
+++ ReptiCare/scripts/main.js
@@ -3,7 +3,7 @@ const form = document.querySelector('#add-animals');
 
 function renderAnimals(doc) {
 let tr = document.createElement('tr');
-let td = document.createElement("td");
+let td = document.createElement('td');
 let deleteButton = document.createElement('button');
 let editButton = document.createElement('button')
 let name = document.createElement('div');
@@ -16,32 +16,32 @@ let basking_area_temp = document.createElement('div');
 let cold_part_temp = document.createElement('div');
 let humidity = document.createElement('div');
 let additional_info = document.createElement('div');
-let editForm = document.createElement("form");
-let submit = document.createElement("input"); 
-let nameInput = document.createElement("input");
-let speciesInput = document.createElement("input");
-let ageSpecies = document.createElement("input");
-let lastFedInput = document.createElement("input");
-let lastShedInput = document.createElement("input");
-let dietInput = document.createElement("input");
-let baskingAreaTempInput = document.createElement("input");
-let coldPartTempInput = document.createElement("input");
-let humidityInput = document.createElement("input");
-let additionalInfoInput = document.createElement("input");
+let editForm = document.createElement('form');
+let submit = document.createElement('input'); 
+let nameInput = document.createElement('input');
+let speciesInput = document.createElement('input');
+let ageSpecies = document.createElement('input');
+let lastFedInput = document.createElement('input');
+let lastShedInput = document.createElement('input');
+let dietInput = document.createElement('input');
+let baskingAreaTempInput = document.createElement('input');
+let coldPartTempInput = document.createElement('input');
+let humidityInput = document.createElement('input');
+let additionalInfoInput = document.createElement('input');
 let modal = document.createElement('div');
 let modalContent = document.createElement('div');
 let span = document.createElement('span');
 let h2 = document.createElement('h2');
 let nameLabel = document.createElement('label')
-let speciesLabel = document.createElement("label");
-let ageLabel = document.createElement("label");
-let lastLabel = document.createElement("label");
-let lastShedLabe = document.createElement("label");
-let dietLabel = document.createElement("label");
-let baskingAreaTempLabel = document.createElement("label");
-let coldPartTempLabel = document.createElement("label");
-let humidityLabel = document.createElement("label");
-let additionalInfoLabel = document.createElement("label");
+let speciesLabel = document.createElement('label');
+let ageLabel = document.createElement('label');
+let lastFedLabel = document.createElement('label');
+let lastShedLabel = document.createElement('label');
+let dietLabel = document.createElement('label');
+let baskingAreaTempLabel = document.createElement('label');
+let coldPartTempLabel = document.createElement('label');
+let humidityLabel = document.createElement('label');
+let additionalInfoLabel = document.createElement('label');
 
 
 tr.setAttribute('data-id', doc.id);
@@ -52,14 +52,21 @@ editButton.textContent = 'Edit';
 editButton.setAttribute('class', 'btn btn-info');
 editForm.setAttribute('class', 'form-group')
 modal.setAttribute('id','editModal');
-modal.setAttribute('class','modal');
-modalContent.setAttribute('class','modal-content');
+modal.setAttribute('class','modal ');
+modalContent.setAttribute('class','modal-content container');
 span.textContent = 'X';
 span.setAttribute('class','close');
 h2.textContent = 'Edit Animal';
 speciesLabel.innerHTML = 'Species:'
 nameLabel.innerHTML = 'Name:'
-
+ageLabel.textContent = 'Age:'
+lastFedLabel.textContent='Last Fed:'
+lastShedLabel.textContent= 'Last Shed:'
+dietLabel.textContent = 'Diet:'
+baskingAreaTempLabel.textContent = 'Basking temps'
+coldPartTempLabel.textContent = 'Cold Part Temps'
+humidityLabel.textContent = 'Humidity'
+additionalInfoInput.textContent = 'Additional Info'
 species.textContent = `species: ${doc.data().species}`;
 age.textContent = `age: ${doc.data().age}`;
 last_fed.textContent = `last fed: ${doc.data().last_fed}`;
@@ -73,16 +80,27 @@ editForm.setAttribute('method',"post");
 editForm.setAttribute('action',"#");
 editForm.setAttribute('class','edit-form')
 submit.setAttribute('type',"submit");
-submit.setAttribute('value',"Submit");
+submit.setAttribute('value',"Update");
 submit.setAttribute('class',"btn btn-success");
 nameLabel.setAttribute('for','name')
 speciesLabel.setAttribute('for','name')
 ageLabel.setAttribute('for','name')
-lastLabel.setAttribute('for','')
-lastShedLabe.setAttribute('for','last shed')
+lastFedLabel.setAttribute('for','')
+lastShedLabel.setAttribute('for','last shed')
 dietLabel.setAttribute('for','diet')
 nameInput.setAttribute('class','form-control')
-speciesInput.s
+speciesInput.setAttribute('class','form-control')
+ageSpecies.setAttribute('class','form-control')
+lastFedInput.setAttribute('class','form-control')
+lastShedInput.setAttribute('class','form-control')
+baskingAreaTempInput.setAttribute('class','form-control')
+coldPartTempInput.setAttribute('class','form-control')
+humidityInput.setAttribute('class','form-control')
+additionalInfoInput.setAttribute('class','form-control')
+speciesInput.setAttribute('class','form-control')
+dietInput.setAttribute('class','form-control')
+
+
 
 
 td.appendChild(species);
@@ -104,19 +122,28 @@ td.append(dietInput);
 td.append(baskingAreaTempInput);
 td.append(coldPartTempInput);
 td.append(humidityInput);
-td.append(additionalInfoInput);
+// td.append(additionalInfoInput);
 td.append(speciesInput)
 tr.appendChild(td);
-editForm.append(speciesLabel)
+editForm.append(nameLabel)
 editForm.appendChild(nameInput);
+editForm.append(ageLabel)
 editForm.append(ageSpecies);
+editForm.append(lastFedLabel)
 editForm.append(lastFedInput);
+editForm.append(lastShedLabel)
 editForm.append(lastShedInput);
+editForm.append(dietLabel);
 editForm.append(dietInput);
+editForm.append(baskingAreaTempLabel)
 editForm.append(baskingAreaTempInput);
+editForm.append(coldPartTempLabel)
 editForm.append(coldPartTempInput);
+editForm.append(humidityLabel)
 editForm.append(humidityInput);
-editForm.append(additionalInfoInput);
+editForm.append(additionalInfoLabel)
+// editForm.append(additionalInfoInput);
+editForm.append(speciesLabel)
 editForm.append(speciesInput)
 editForm.append(submit)
 td.append(editForm);
