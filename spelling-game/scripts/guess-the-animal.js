b7c7b7b9d4043f113e3e85a0f5f294483618b4cf
Philip Kovachev <pkova@softserveinc.com>
2019-01-18 10:57:33 +0200

    Fixed the double increment bug

--- spelling-game/scripts/guess-the-animal.js
+++ spelling-game/scripts/guess-the-animal.js
@@ -16,25 +16,27 @@ var sounds = [
 var player = document.getElementById('player');
 var enteredWord = document.getElementById('entered-word');
 var counter = document.getElementById('counter-score');
+var errorMessage = document.getElementById('error-message');
 
 startGame();
 
-
-  function startGame() {
+function startGame() {
+    var currentSound;
     player.addEventListener('click', function() {
-      var sound = sounds[Math.floor(Math.random()*sounds.length)];
-      var currentSound = sound.animalType;
-      sound['sound'].play();
+    var sound = sounds[Math.floor(Math.random()*sounds.length)];
+    currentSound = sound.animalType;
+    sound['sound'].play();
+  })
 
-      enteredWord.addEventListener('keydown', function() {
-        if(event.key === 'Enter') {
-        if(enteredWord.value === currentSound) {
-            counter.textContent ++;
-         }
-        }
-      })
-    })
-  }
+
+  enteredWord.addEventListener('keydown', function() {
+    if(event.key === 'Enter') {
+      if(enteredWord.value === currentSound) {
+        counter.textContent ++;
+      }
+    }
+  })
+}
   
 
 
