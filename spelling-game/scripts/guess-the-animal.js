0270259bb76d3b18acdb8fafb2c4a6569b159476
Philip Kovachev <pkova@softserveinc.com>
2019-01-18 12:29:03 +0200

    Added more animal sounds, updated the validation and edited the secondary help modal height

--- spelling-game/scripts/guess-the-animal.js
+++ spelling-game/scripts/guess-the-animal.js
@@ -10,6 +10,26 @@ var sounds = [
   {
     animalType: 'goat',
     sound: new Audio('../sounds/Goat-noise.mp3'),
+  },
+  {
+    animalType: 'cat',
+    sound: new Audio('../sounds/cat.mp3')
+  },
+  {
+    animalType: 'dog',
+    sound: new Audio('../sounds/dog-bark.mp3')
+  },
+  {
+    animalType: 'sheep',
+    sound: new Audio('../sounds/sheep.mp3')
+  },
+  {
+    animalType: 'rooster',
+    sound: new Audio('../sounds/rooster.mp3')
+  },
+  {
+    animalType: 'lion',
+    sound: new Audio('../sounds/lion.mp3')
   }
 ]
 
@@ -17,7 +37,7 @@ var player = document.getElementById('player');
 var enteredWord = document.getElementById('entered-word');
 var counter = document.getElementById('counter-score');
 var errorMessage = document.getElementById('error-message');
-
+var wrongAnswerSound = new Audio('../sounds/Wrong-answer-sound-effect.mp3');
 
 startGame();
 
@@ -39,6 +59,7 @@ function startGame() {
       } else {
         errorMessage.style.display = 'inline-block';
         enteredWord.classList.add('input-error-border');
+        wrongAnswerSound.play();
       } 
     }
   })
