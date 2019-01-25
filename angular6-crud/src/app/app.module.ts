928b9a6f86f2597a561f3200bd8670b4cffe4bb3
Philip Kovachev <pkova@softserveinc.com>
2019-01-25 16:17:18 +0200

    The barebones of the project are ready + form validation

---
+++ angular6-crud/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { ReactiveFormsModule } from '@angular/forms'
+import { CustomerService } from './shared/customer.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    CustomerListComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule 
+  ],
+  providers: [CustomerService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
